503e113410b05f3c93f089f9508cf67c90e6aa0a
drakosha <drakosha114@gmail.com>
2021-06-02 00:13:35 +0300

    Section 2 - Building a walking sceleton - API

---
+++ API/Controllers/UsersController.cs
@@ -0,0 +1,35 @@
+﻿using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        public UsersController(DataContext context) : base()
+        {
+            Context = context;
+        }
+
+        public DataContext Context { get; }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
+        {
+            return await Context.Users.ToListAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<AppUser>> GetUser(int id) {
+            return await Context.Users.FindAsync(id);
+        }
+    }
+}
