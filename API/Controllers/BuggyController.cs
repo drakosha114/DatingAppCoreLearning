e08d3126f442802e2883219b7c88734f921a9f95
Andrey <drakosha114@gmail.com>
2021-10-27 00:20:32 +0300

    BE part - Exception Middleware, controller for errors testing. (#8)

---
+++ API/Controllers/BuggyController.cs
@@ -0,0 +1,62 @@
+﻿using API.Data;
+using API.DTOs;
+using API.Entities;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Controllers
+{
+    public class BuggyController : BaseApiController
+    {
+        public BuggyController(DataContext context)
+        {
+            Context = context;
+        }
+
+        private DataContext Context { get; }
+
+        [Authorize]
+        [HttpGet("auth")]
+        public ActionResult<string> GetSeecret() {
+            return "Secret text";
+        }
+
+        [HttpGet("not-found")]
+        public ActionResult<AppUser> GetNotFound()
+        {
+            var user = Context.Users.Find(-1);
+
+            if (user == null) {
+                return NotFound();
+            }
+            return Ok(user);
+        }
+
+        [HttpGet("internal-error")]
+        public ActionResult<string> GetInternalError() {
+
+            var user = Context.Users.Find(-1);
+
+            var stringToReturn = user.ToString();
+
+            return stringToReturn;
+        }
+
+
+        [HttpGet("bad-request")]
+        public ActionResult<string> GetBadRequest()
+        {
+            return BadRequest("Bad request result");
+        }
+
+        [HttpPost("validation-error")]
+        public ActionResult<RegisterDTO> GetValidationError(RegisterDTO registerData) {
+
+            return Ok(registerData);
+        }
+    }
+}
