61efa8fdb0ea60df97a34aac8a14710ce8af1f79
Andrey <drakosha114@gmail.com>
2021-11-07 21:04:21 +0200

    Section 7 error handling/root (#11)
    * BE part - Exception Middleware, controller for errors testing. (#8)
    
    * Section 7 error handling/fe part (#10)
    
    * fix - added base class for macro command
    
    * Section 6 angular routing/notifications service (#4)
    
    * Added toaster service
    Decorators with notifications for observable state handling
    
    * LoginContainerComponent, RegisterContainerComponent, PrivateRoutesRootScreenComponent. Destroy subscriptions after components destroying.
    
    * Section 6 angular routing/main menu user ment page not found (#5)
    
    * User menu component, user profile module
    
    * bootswatch package added. Added dark bootstrap theme.
    
    * Main menu
    
    * Section 6 angular routing/main menu user ment page not found (#7)
    
    * User menu component, user profile module
    
    * bootswatch package added. Added dark bootstrap theme.
    
    * Main menu
    
    * Refactoring - member module moved to members module as lazy-loaded module
    
    * 404 page styles
    layout styles

--- client/src/app/shared/layout/components/content/content.component.ts
+++ client/src/app/shared/layout/components/content/content.component.ts
@@ -1,4 +1,4 @@
-import {Component, HostBinding, Input, OnInit, ViewEncapsulation} from '@angular/core';
+import {Component, HostBinding, Input, OnInit} from '@angular/core';
 import {Breakpoints} from "../../../types";
 
 @Component({
